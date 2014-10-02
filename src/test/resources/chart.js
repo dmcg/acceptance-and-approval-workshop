af994b03753fc3df927aabace46777d25e1d6cd6
Duncan McGregor <duncan@oneeyedmen.com>
2014-10-02 13:21:29 +0100

    Fix charting stuff broken by browser update

--- src/test/resources/chart.js
+++ src/test/resources/chart.js
@@ -1,16 +1,26 @@
-
 function datapoints(id) {
-    return d3.selectAll("#"+id+" tr").selectAll("td").map(function(d) {
-	return {x: parseFloat(d[0].textContent), y: parseFloat(d[1].textContent)};
-    });
+//    var result = [];
+//    var rows = document.getElementById(id).getElementsByTagName('tr')
+//    for (var i = 0; i < rows.length; i++) {
+//        var d = rows[i].getElementsByTagName("td");
+//        result.push({x: parseFloat(d[0].textContent), y: parseFloat(d[1].textContent)})
+//    }
+//    return rows;
+    var result = d3.selectAll("#" + id + " tr").selectAll("td").map(
+        function (d) {
+            return {
+                x: parseFloat(d[0].textContent),
+                y: parseFloat(d[1].textContent)};
+        });
+    return result;
 }
 
-var history = datapoints("history");
-var projection = datapoints("projection");
+var historyPoints = datapoints("history");
+var projectionPoints = datapoints("projection");
 
-var chart_data = [{key: "History", values: history}];
+var chart_data = [{key: "History", values: historyPoints}];
 if (projection.length > 0) {
-    chart_data.push({key: "Projection", values: projection});
+    chart_data.push({key: "Projection", values: projectionPoints});
 }
 
 var chart = nv.models.lineChart()
@@ -29,7 +39,7 @@ chart.yAxis
     .axisLabel('Parts per Million')
     .tickFormat(d3.format('f'));
 
-d3.select('#plot svg')
+d3.select('#svg')
     .datum(chart_data)
     .call(chart);
 
